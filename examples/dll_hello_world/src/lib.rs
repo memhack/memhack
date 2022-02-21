ebb8060dad661baf4a62716f619fd282c5807ac7
Jakob <31919330+Jakobzs@users.noreply.github.com>
2022-02-21 21:08:22 +0100

    doc

--- examples/dll_hello_world/src/lib.rs
+++ examples/dll_hello_world/src/lib.rs
@@ -6,6 +6,7 @@ use windows::Win32::{
     },
 };
 
+// Entry point for the Windows DLL, to be injected using a dll injector
 #[no_mangle]
 #[allow(unused_variables)]
 pub extern "stdcall" fn DllMain(module: HINSTANCE, reason: u32, reserved: *mut c_void) -> BOOL {
@@ -20,6 +21,7 @@ pub extern "stdcall" fn DllMain(module: HINSTANCE, reason: u32, reserved: *mut c
     BOOL::from(true)
 }
 
+// The start function for the dll. In this example it is simply printing out "Hello world!"
 fn start(_module: HINSTANCE) {
     if let Err(err) = memhack::open_debug_console() {
         println!("Error opening console: {}", err);
